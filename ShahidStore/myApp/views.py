84fe2a3c6e0217a743dcfff5b77f1d361ad1d4c2
Shahid Khan <shahidkhan88892@gmail.com>
2026-07-07 23:08:40 +0530

    created catalog page and modified it's endpoint

--- ShahidStore/myApp/views.py
+++ ShahidStore/myApp/views.py
@@ -6,5 +6,13 @@ def home(request):
     return render(request, 'home_page.html')
 
 def catalog(request):
+    CATEGORY_MAP = {
+    "Electronics": ["electronics_gaming","electronics_mobile","electronics_camera",],
+    "Fashion": ["apparel","footwear","bags","fashion_accessories",],
+    "Beauty": ["beauty_skincare", ],
+    "Home": ["furniture_home","kitchen_dining",],
+    "Sports": ["sports_outdoors",],
+}
+    
     products=Product.objects.all()
-    return render(request, 'products.html',{"products":products})
+    return render(request, 'catalog.html',{"products":products,"categories":CATEGORY_MAP.keys()})
